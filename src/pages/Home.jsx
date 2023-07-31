c89289a21de387ffe002d341057f85dad7ae43fc
mimcmahon20 <mimcmahon20@vt.edu>
2023-07-31 19:48:52 -0400

    Header component & stylesheet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import Header from "../components/Header";
+
 export default function Home() {
-  return (<div>Home</div>);
+  return (<div className="homepage">
+    <Header />
+  </div>);
 }
