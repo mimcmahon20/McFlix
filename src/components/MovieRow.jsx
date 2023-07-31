ece7cc8f315fb351fce30fc41a6f20e86fede5de
mimcmahon20 <mimcmahon20@vt.edu>
2023-07-31 19:50:59 -0400

    MovieRow component & styling

---
+++ src/components/MovieRow.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles/MovieRow.css";
+
+export default function MovieRow() {
+  return (<div>MovieRow</div>);
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import Header from "../components/Header";
+import MovieRow from "../components/MovieRow";
 
 export default function Home() {
   return (<div className="homepage">
     <Header />
+    <MovieRow />
   </div>);
 }
