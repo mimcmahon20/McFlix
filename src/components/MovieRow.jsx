22a50d08db278d1ab6939b8f7af44c5da318b489
mimcmahon20 <mimcmahon20@vt.edu>
2023-07-31 23:48:38 -0400

    Styling movie row

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import "../styles/MovieRow.css";
+import arrow from "../assets/arrow.svg";
 
 import Movie from "./Movie";
 
-export default function MovieRow() {
 
-    // let movies = [{
-    //     id: 0,
-    //     name: "Avengers: Infinity War",
-    //     overview: "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos.",
-    //     release_date: "2018-04-25",
-    //     poster_path: "https://image.tmdb.org/t/p/w300_and_h450_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
-    // },
-    // {
-    //     id: 1,
-    //     name: "The Avengers",
-    //     overview: "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster.",
-    //     release_date: "2012-04-25",
-    //     poster_path: "https://image.tmdb.org/t/p/w300_and_h450_bestv2/cezWGskPY5x7GaglTTRN4Fugfb8.jpg"
-    // }];
-  return (<div className="slider">
-    <Movie />
-    <Movie />
-  </div>);
+export default function MovieRow() {
+  // let movies = [{
+  //     id: 0,
+  //     name: "Avengers: Infinity War",
+  //     overview: "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos.",
+  //     release_date: "2018-04-25",
+  //     poster_path: "https://image.tmdb.org/t/p/w300_and_h450_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
+  // },
+  // {
+  //     id: 1,
+  //     name: "The Avengers",
+  //     overview: "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster.",
+  //     release_date: "2012-04-25",
+  //     poster_path: "https://image.tmdb.org/t/p/w300_and_h450_bestv2/cezWGskPY5x7GaglTTRN4Fugfb8.jpg"
+  // }];
+  return (
+    <div className="row">
+      <h2 className="row-title">My List</h2>
+      <div className="slider">
+        <div className="arrow">
+          <img src={arrow} alt='arrow'></img>
+        </div>
+        <Movie />
+        <Movie />
+        <div className="arrow">
+          <img src={arrow} alt='arrow'></img>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,8 @@ export default function Home() {
   return (<div className="homepage">
     <Header />
     <MovieRow />
+    <MovieRow />
+    <MovieRow />
 
   </div>);
 }
