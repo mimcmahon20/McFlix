5b6b47ea6afec35dcd542eef6d41e81502eb86d7
mimcmahon20 <mimcmahon20@vt.edu>
2023-08-10 16:35:07 -0400

    Introducing the description section on hover

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -8,6 +8,13 @@ return (<div className="movie-container-padded">
     <div className="movie-container">
       <div className="movie-poster">
         <img src={movie.src} alt="movie poster"></img>
+        <div className="movie-description">
+          <div className="description-content">
+            <div className="movie-buttons"></div>
+            <div className="movie-info-description"></div>
+            <div className="movie-genre"></div>
+          </div>
+        </div>
         </div>
       </div>
   </div>);
